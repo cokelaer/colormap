f9f5b066d8c057958f2af37bd2b64f67b8290b56
Thomas Cokelaer <cokelaer@gmail.com>
2021-10-16 22:05:14 +0200

    update metadata

--- setup.py
+++ setup.py
@@ -27,12 +27,10 @@ metainfo = {
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Software Development :: Libraries :: Python Modules'
           ]
     }
